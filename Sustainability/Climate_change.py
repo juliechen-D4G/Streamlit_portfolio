61d627752828af2eca104636fc001467ecbce4a0
juliechen-D4G <juliechrystelle.chen@gmail.com>
2026-03-20 13:31:55 -0300

    Update Climate_change.py

--- Sustainability/Climate_change.py
+++ Sustainability/Climate_change.py
@@ -1,18 +1,4 @@
-# Page Configuration
-st.set_page_config(
-    page_title="Climate Change | Data Viz",
-    page_icon="🌍",
-    layout="wide"
-)
-
-# --- CUSTOM STYLE ---
-st.markdown("""
-    <style>
-    .main {
-        background-color: #f8f9fa;
-    }
-    .stBadge {
-        background-color: #e9ecef;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+import streamlit as st
+# Page Title and Intro
+st.title("Climate Change| Data Viz"),
+st.write("In this page we will explore Climate Change data")
