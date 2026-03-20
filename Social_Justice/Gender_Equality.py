f83dbe3c3fc9427a992213b37ead62262ef09a98
juliechen-D4G <juliechrystelle.chen@gmail.com>
2026-03-20 13:31:08 -0300

    Update Gender_Equality.py

--- Social_Justice/Gender_Equality.py
+++ Social_Justice/Gender_Equality.py
@@ -1,18 +1,5 @@
-# Page Configuration
-st.set_page_config(
-    page_title="Gender Equality | Data Viz",
-    page_icon="♀",
-    layout="wide"
-)
+import streamlit as st
+# Page Title and Intro
+st.title("♀ Gender Equality | Data Viz"),
+st.write("In this page we will explore gender equality data")
 
-# --- CUSTOM STYLE ---
-st.markdown("""
-    <style>
-    .main {
-        background-color: #f8f9fa;
-    }
-    .stBadge {
-        background-color: #e9ecef;
-    }
-    </style>
-    """, unsafe_allow_html=True)
