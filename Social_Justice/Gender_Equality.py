ba4cf0405937df934edec473c8177a26fcb2465a
juliechen-D4G <juliechrystelle.chen@gmail.com>
2026-03-20 11:51:15 -0300

    Create Gender_Equality.py

---
+++ Social_Justice/Gender_Equality.py
@@ -0,0 +1,18 @@
+# Page Configuration
+st.set_page_config(
+    page_title="Gender Equality | Data Viz",
+    page_icon="♀",
+    layout="wide"
+)
+
+# --- CUSTOM STYLE ---
+st.markdown("""
+    <style>
+    .main {
+        background-color: #f8f9fa;
+    }
+    .stBadge {
+        background-color: #e9ecef;
+    }
+    </style>
+    """, unsafe_allow_html=True)
